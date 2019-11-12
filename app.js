bef76d31b5004c0f6b06fff24acc88d7d6b228c5
Emil Sundberg <emilsundberg@Emils-MacBook-Pro.local>
2019-11-12 10:19:59 +0100

    Added file picker and markdown to tinymce

--- app.js
+++ app.js
@@ -4,14 +4,45 @@
 
 tinymce.init({
     selector: '#note-body',
-    toolbar: 'bold italic strikethrough underline h2 h3 | bullist numlist blockquote image link | print',
+    toolbar: 'bold italic strikethrough underline h2 h3 | bullist numlist blockquote image link print wordcount |',
     fixed_toolbar_container: '#note-toolbar',
     toolbar_drawer: 'sliding',
     menubar: false,
     branding: false,
     inline: true,
     skin: 'oxide-dark',
-    plugins: 'lists image link print',
+    plugins: 'lists image code link print textpattern wordcount',
+
+    //file picker image
+    image_title: true, 
+    // enable automatic uploads of images represented by blob or data URIs
+    automatic_uploads: true,
+    // add custom filepicker only to Image dialog
+    file_picker_types: 'image',
+    file_picker_callback: function(cb, value, meta) {
+      var input = document.createElement('input');
+      input.setAttribute('type', 'file');
+      input.setAttribute('accept', 'image/*');
+  
+      input.onchange = function() {
+        var file = this.files[0];
+        var reader = new FileReader();
+        
+        reader.onload = function () {
+          var id = 'blobid' + (new Date()).getTime();
+          var blobCache =  tinymce.activeEditor.editorUpload.blobCache;
+          var base64 = reader.result.split(',')[1];
+          var blobInfo = blobCache.create(id, file, base64);
+          blobCache.add(blobInfo);
+  
+          // call the callback and populate the Title field with the file name
+          cb(blobInfo.blobUri(), { title: file.name });
+        };
+        reader.readAsDataURL(file);
+      };
+      
+      input.click();
+    }
 });
 
 // ############
