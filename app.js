e89364bf07efdd5652067398cab645875ac64e63
fredrik1998 <fredrik.andren@student.kyh.se>
2019-11-25 11:00:20 +0100

    Tags

--- app.js
+++ app.js
@@ -371,7 +371,7 @@ const renderNotesList = () => {
                     <div class="note-list-meta-container">
                         <time datetime="${noteDateISO}" class="note-list-date">${noteDate}</time>
                         <div class="note-list-icons">
-                            <button class= "delete-button" title="Delete note"></button>
+                            <button class= "delete-button" title="Delete note"></button >
                             <input type="checkbox" name="star-${note.id}" id="star-${note.id}" class="starred-checkbox" ${note.starred ? "checked" : ""}>
                             <label for="star-${note.id}" title="${note.starred ? 'Unstar note' : 'Star note'}"></label>
                         </div>
@@ -741,6 +741,8 @@ templateContent.addEventListener('click', (e) => {
     }
 })
 
+
+
 const searchInput = document.querySelector('#search');
 const searchStarred = document.querySelector('#starred-search');
 
@@ -807,13 +809,78 @@ const searchNotesList = (searchString = '', searchStar = false) => {
                         </div>
                     </div>
                     <a href="#" class="note-list-link"><h3 class="note-list-title">${noteTitle}</h3></a>
+                   <div class="tag-container">
+                   <input/>  
+                   </div>
                     <span class="note-list-preview">${notePreview}</span>
                 </li>`;
         }
+
+     
+
+const tagContainer = document.querySelector('.tag-container');
+const input = document.querySelector('.tag-container input');
+
+let tags = [];
+
+function createTag(label) {
+  const div = document.createElement('div');
+  div.setAttribute('class', 'tag');
+  const span = document.createElement('span');
+  span.innerHTML = label;
+  const closeIcon = document.createElement('i');
+  closeIcon.innerHTML = 'close';
+  closeIcon.setAttribute('class', 'material-icons');
+  closeIcon.setAttribute('data-item', label);
+  div.appendChild(span);
+  div.appendChild(closeIcon);
+  return div;
+}
+
+function clearTags() {
+  document.querySelectorAll('.tag').forEach(tag => {
+    tag.parentElement.removeChild(tag);
+  });
+}
+
+function addTags() {
+  clearTags();
+  tags.slice().reverse().forEach(tag => {
+    tagContainer.prepend(createTag(tag));
+  });
+}
+
+input.addEventListener('keyup', (e) => {
+    if (e.key === 'Enter') {
+      e.target.value.split(',').forEach(tag => {
+        tags.push(tag);  
+      });
+      
+      addTags();
+      input.value = '';
+    }
+});
+document.addEventListener('click', (e) => {
+  console.log(e.target.tagName);
+  if (e.target.tagName === 'I') {
+    const tagLabel = e.target.getAttribute('data-item');
+    const index = tags.indexOf(tagLabel);
+    tags = [...tags.slice(0, index), ...tags.slice(index+1)];
+    addTags();    
+  }
+})
+
+input.focus();
+
+        
+
     })
     if (notesList.innerHTML == '') {
         notesList.innerHTML =
             `<div class="note-list-no-results"><i>No ${(searchStar) ? "starred" : ""} notes 
         ${(searchString) ? 'with ' + '"' + searchString + '"' : ""} found.</i ></div>`;
+        
     }
 }
+
+
